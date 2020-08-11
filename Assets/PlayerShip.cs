764314fc6421182a17ba8ed0ab1afd6007c851ca
Tommy Morris <konicm8ker@gmail.com>
2020-08-11 01:48:12 -0500

    Added rotation to player ship when via input controls. (16_aa_cu2)

--- Assets/PlayerShip.cs
+++ Assets/PlayerShip.cs
@@ -5,24 +5,52 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerShip : MonoBehaviour
 {
-    [Tooltip("In m/s^-1")][SerializeField] float xSpeed = 4f;
-    [Tooltip("In m/s^-1")][SerializeField] float ySpeed = 4f;
-    [Tooltip("In m")][SerializeField] float xRange = 10f;
-    [Tooltip("In m")][SerializeField] float yRange = 5f;
+    [Tooltip("In m/s^-1")][SerializeField] float speed = 20f;
+    [Tooltip("In m")][SerializeField] float xRange = 12f;
+    [Tooltip("In m")][SerializeField] float yRange = 8f;
+    [SerializeField] float posPitchFactor = -3f;
+    [SerializeField] float posYawFactor = 3f;
+    [SerializeField] float controlPitchFactor = -20f;
+    [SerializeField] float controlRollFactor = -20f;
 
+    float xThrow, yThrow;
 
     void Update()
     {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float xOffset = xThrow * xSpeed * Time.deltaTime; // Gets current x offset per frame
-        float rawXPos = Mathf.Clamp(transform.localPosition.x + xOffset, -xRange, xRange); // Prevents going offscreen on x axis
+        ProcessTranslation();
+        ProcessRotation();
+    }
 
-        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
-        float yOffset = yThrow * ySpeed * Time.deltaTime; // Gets current y offset per frame
+    private void ProcessTranslation()
+    {
+
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        Debug.Log("X Throw: " + xThrow + " | Y Throw: " + yThrow );
+
+        float xOffset = xThrow * speed * Time.deltaTime; // Gets current x offset per frame
+        float yOffset = yThrow * speed * Time.deltaTime; // Gets current y offset per frame
+        float rawXPos = Mathf.Clamp(transform.localPosition.x + xOffset, -xRange, xRange); // Prevents going offscreen on x axis
         float rawYPos = Mathf.Clamp(transform.localPosition.y + yOffset, -yRange, yRange); // Prevents going offscreen on y axis
 
+        transform.localPosition = new Vector3(rawXPos, rawYPos, transform.localPosition.z);
+    }
 
-        transform.localPosition = new Vector3(rawXPos, transform.localPosition.y, transform.localPosition.z);
-        transform.localPosition = new Vector3(transform.localPosition.x, rawYPos, transform.localPosition.z);
+    private void ProcessRotation()
+    {
+        // Rotates player ship based on pos and throw on y axis
+        float pitchDueToPos = transform.localPosition.y * posPitchFactor;
+        float pitchDueToThrow = yThrow * controlPitchFactor;
+        float pitch =  pitchDueToPos + pitchDueToThrow;
+
+        // Rotates player ship based on pos on x axis
+        float yawDueToPos = transform.localPosition.x * posYawFactor;
+        float yaw = yawDueToPos;
+
+        // Rotates player ship based on throw on x axis
+        float rollDueToThrow = xThrow * controlRollFactor;
+        float roll = rollDueToThrow;
+
+        transform.localRotation = Quaternion.Euler(pitch,yaw,roll);
     }
 }
