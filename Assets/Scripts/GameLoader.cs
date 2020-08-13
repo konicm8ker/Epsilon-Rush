f663be66d7ec497e45ecb3d051680e33e9c5ab9b
Tommy Morris <konicm8ker@gmail.com>
2020-08-12 20:42:48 -0500

    Organized project structure and optimized code. (21_aa_cu2)

---
+++ Assets/Scripts/GameLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameLoader : MonoBehaviour
+{
+    void Start()
+    {
+        Invoke("LoadFirstLevel", 4f);
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
