27106b785b7a22b6576117849e1275d7fefca030
Tommy Morris <konicm8ker@gmail.com>
2020-08-28 04:46:40 -0500

    Finished adding voice overs and a few sfx. (40_aa_cu2)

---
+++ Assets/Scripts/VoiceOverIntro.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VoiceOverIntro : MonoBehaviour
+{
+    AudioSource audioSource;
+    public GameObject holoAvatar;
+    
+
+    void Awake()
+    {
+        PreserveIntroObjects(this.gameObject);
+        PreserveIntroObjects(holoAvatar);
+    }
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        Invoke("PlayIntroVO", 1.5f);
+    }
+
+    void Update()
+    {
+        if(Time.time > 6f)
+        {
+            DestroyIntroObjects(this.gameObject);
+            DestroyIntroObjects(holoAvatar);
+        }
+    }
+
+    private void PlayIntroVO()
+    {
+        audioSource.Play();
+        holoAvatar.SetActive(true);
+    }
+
+    private void PreserveIntroObjects(GameObject value)
+    {
+        DontDestroyOnLoad(value);
+    }
+
+    private void DestroyIntroObjects(GameObject value)
+    {
+        Destroy(value);
+    }
+}
